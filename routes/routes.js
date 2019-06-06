acd04d8392ad0b2b8cc411af98b6b40bfb7ac533
Pano Skylakis <pano_skylakis@outlook.com>
2019-06-07 08:28:59 +1200

    Bug fix
    Entering anything after /profile/ or /edit/ no longer displays the
    main.hbs page

--- routes/routes.js
+++ routes/routes.js
@@ -11,10 +11,15 @@ const profileData = {
 
 profileRoute.get('/profile/:id', (req, res) => {
     let danProfile = profiles.users.find(item => {
-        req.params.id === item.id
+        return req.params.id == item.id
     })
-    // console.log(danProfile)
-    res.send(`This is profile number ${req.params.id}`)
+    console.log(danProfile)
+    if (danProfile != undefined) {
+        res.render('./dans/profile', danProfile)
+    } else {
+        res.send('<h1><strong>404:</strong> This Dan does not exist</h1>')
+    }
+    
 })
 
 
@@ -22,7 +27,12 @@ profileRoute.get('/edit/:id', (req, res) => {
     let danProfile = profiles.users.find(item => {
         return req.params.id == item.id;
     })
-    res.send(`This is the edit page for ${req.params.id}`)
+    console.log(danProfile)
+    if (danProfile != undefined) {
+        res.render('./dans/edit', danProfile)
+    } else {
+        res.send('<h1><strong>404:</strong> This Dan does not exist</h1>')
+    }
 })
 
 
--- server.js
+++ server.js
@@ -15,7 +15,6 @@ server.use(express.urlencoded({extended: true}))
 
 
 server.get('/', (req, res) =>{
-  // res.send('This is the index page')
   res.render('./dans/index', profiles)
 })
 
